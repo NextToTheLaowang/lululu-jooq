753f345b203826b2ee229c0162eb6c371b2457e5
wangqiyue <18611310042@163.com>
2019-03-12 09:36:09 +0800

    first commit

---
+++ src/test/java/com/java/src/lululu/LululuApplicationTests.java
@@ -0,0 +1,17 @@
+package com.java.src.lululu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LululuApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
+
