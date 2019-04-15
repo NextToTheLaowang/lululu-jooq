23bfb6f8e5fdad04a661728615ed9823243a075d
liuyuan <liuyuan@317hu.com>
2019-04-15 13:54:30 +0800

    test commit

---
+++ src/main/java/com/java/src/lululu/common/util/UUIDUtil.java
@@ -0,0 +1,18 @@
+package com.java.src.lululu.common.util;
+
+import java.util.UUID;
+
+/**
+ * Created by liuyuan on 2019/4/15.
+ */
+public class UUIDUtil {
+
+    public static String getUUID(){
+        return UUID.randomUUID().toString().replace("-", "");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("格式前的UUID ： " + UUID.randomUUID().toString());
+        System.out.println("格式化后的UUID ：" + getUUID());
+    }
+}
